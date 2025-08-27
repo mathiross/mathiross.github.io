8175fa2a1e3cf730c590ce6befb871bfb7c30a03
mathiross <37271791+mathiross@users.noreply.github.com>
2025-08-27 12:36:13 +0200

    Update invoice.js

--- invoice.js
+++ invoice.js
@@ -14,7 +14,7 @@ function addDemo(row) {
     for (const key of ['Subtotal', 'Deduction', 'Taxes', 'Total']) {
       if (!(key in row)) { row[key] = key; }
     }
-    if (!('Note' in row)) { row.Note = '(Anything in a Note column goes here)'; }
+    if (!('Note' in row)) { row.Note = '(Tout ce qui se trouve dans la colonne Note se retrouve ici.)'; }
   }
   if (!row.Invoicer) {
     row.Invoicer = {
